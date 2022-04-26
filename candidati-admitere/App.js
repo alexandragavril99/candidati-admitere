c99963769c3796c832b78d6cfc8c50411b3445ba
alexandragavril99 <alexandragavril19@gmail.com>
2022-04-26 14:55:16 +0300

    First page

--- candidati-admitere/App.js
+++ candidati-admitere/App.js
@@ -1,15 +1,49 @@
-import { View, Text } from "react-native";
+import { View, Button, StyleSheet, ImageBackground } from "react-native";
+// import { useHistory } from "react-router-native";
 
 export default function App() {
+  // const history = useHistory();
+
+  // function navigateToAllCandidates() {
+  //   history.push("candidates");
+  // }
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Universal React with Expo</Text>
+    <View style={styles.container}>
+      <ImageBackground
+        source={require("./assets/school.jpg")}
+        style={styles.image}
+      />
+      <View style={styles.buttonContainer}>
+        <Button
+          title="Vizualizare candidați"
+          style={styles.button}
+          // onPress={navigateToAllCandidates()}
+        />
+        <View style={styles.space} />
+        <Button title="Adăugare candidat" style={styles.button} />
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  buttonContainer: {
+    width: 250,
+  },
+  button: {},
+  space: {
+    width: 20,
+    height: 20,
+  },
+  image: {
+    width: "100%",
+    height: "100%",
+    resizeMode: "cover",
+    position: "absolute",
+  },
+});
---
+++ candidati-admitere/pages/Candidates.js
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export default function Candidates() {
+  return (
+    <View>
+      <Text>ceva</Text>
+    </View>
+  );
+}
